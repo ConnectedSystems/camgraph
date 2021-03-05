e709f3c66314a27ddebffa50949b49f884207798
ConnectedSystems <takuyai@gmail.com>
2021-03-05 17:17:13 +1100

    Added tentative calibration data
    Optimization approach does not work but getting there.

--- examples/optimization.jl
+++ examples/optimization.jl
@@ -2,10 +2,8 @@ using DataFrames, CSV
 using Statistics
 using BlackBoxOptim
 
-using ModelParameters
-
 using Infiltrator
-
+using ModelParameters
 
 
 include("./network_creation.jl")
@@ -35,7 +33,8 @@ function obj_func(params)
 
     node = get_prop(mg, v_id, :node)
     node = deepcopy(node)
-    set_prop!(mg, v_id, :node, ModelParameters.update(node, params))
+    update_params!(node, params)  # node = ModelParameters.update(node, params)
+    set_prop!(mg, v_id, :node, node)
 
     timesteps = sim_length(climate)
     for ts in (1:timesteps)
@@ -46,8 +45,6 @@ function obj_func(params)
     hist_levels = hist_dam_levels[:, "Dam Level [mAHD]"]
     score = -(1.0 - sum((node.level .- hist_levels).^2) / sum((hist_levels .- mean(hist_levels)).^2))
 
-    @infiltrate
-
     # reset to clear stored values
     # reset!(get_prop(mg, v_id, :node))
 
@@ -55,12 +52,22 @@ function obj_func(params)
 end
 
 
-function calibrate(mg, v_id)
-    target_node = Model(get_prop(mg, v_id, :node))
-    @info "Calibrating:" get_prop(mg, v_id, :name)
-    @info target_node
+function calibrate(g, mg, v_id)
 
-    score = bboptimize(obj_func; SearchRange=collect(target_node.bounds))
+    inlets = inneighbors(g, v_id)
+    if !isempty(inlets)
+        for ins in inlets
+            calibrate(g, mg, ins)
+        end
+    end
+
+    @infiltrate
+
+    # Create new optimization function
+    opt_func = x -> obj_func(x, climate, mg, g, v_id, discharge, levels)
+
+    target_node = Model(get_prop(mg, v_id, :node))
+    score = bboptimize(opt_func; SearchRange=collect(target_node.bounds))
 
     return score
 end
@@ -68,8 +75,6 @@ end
 
 match = collect(filter_vertices(mg, :name, "406000"))
 v_id = match[1]
-target_node = Model(get_prop(mg, v_id, :node))
+# target_node = Model(get_prop(mg, v_id, :node))
 
-@info "Before call to function" target_node
-
-@info calibrate(mg, v_id)
+@info calibrate(g, mg, v_id)
