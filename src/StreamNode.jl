5e8adfed4e7223e4f6c5b86c095c331a123342d9
ConnectedSystems <takuyai@gmail.com>
2021-03-05 16:44:47 +1100

    Typing info

--- src/StreamNode.jl
+++ src/StreamNode.jl
@@ -38,8 +38,10 @@ Base.@kwdef mutable struct StreamNode{A} <: NetworkNode{A}
 end
 
 
-function StreamNode(node_id, area, d, d2, e, f, a, b, s_coef, alpha, store::Float64, quick::Float64, slow::Float64)
-    return StreamNode{Float64}(
+function StreamNode(node_id::String, area::Float64, d::Float64, d2::Float64, e::Float64, f::Float64, 
+                    a::Float64, b::Float64, s_coef::Float64, alpha::Float64, 
+                    store::Float64, quick::Float64, slow::Float64)
+    return StreamNode(
         node_id=node_id,
         area=area,
         d=d,
